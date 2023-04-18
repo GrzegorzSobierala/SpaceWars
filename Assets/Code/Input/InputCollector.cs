c9b67a5404ff65b1f75496c9d91af59372fdbe6b
Grzegorz Sobierała <gsobierala@gmail.com>
2023-04-18 21:00:27 +0200

    Input start

---
+++ Assets/Code/Input/InputCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+namespace Game
+{
+    public class InputCollector : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
---
+++ Assets/Code/Input/InputInstaller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+    public class InputInstaller : MonoInstaller<InputInstaller>
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
