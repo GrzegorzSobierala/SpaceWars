2666d457bd638a369a6027e4782e9ed18ee311db
Grzegorz Sobierała <gsobierala@gmail.com>
2023-11-11 22:40:03 +0100

    Test enemy shooting

---
+++ Assets/Code/Gameplay/Enemies/Derived/EnemyBullet.cs
@@ -0,0 +1,49 @@
+using Game.Combat;
+using Game.Player.Ship;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Room.Enemy
+{
+    public class EnemyBullet : ShootableObjectBase
+    {
+        public float MaxDistance => _maxDistance;
+
+        public override void Shoot(Rigidbody2D creatorBody, Transform gunTransform)
+        {
+            gameObject.SetActive(true);
+
+            _body.position = gunTransform.position;
+            _body.rotation = gunTransform.rotation.eulerAngles.z;
+
+            float targetForce = _speed * _body.mass;
+            _body.AddRelativeForce(Vector2.up * targetForce, ForceMode2D.Impulse);
+
+            _shootTime = Time.time;
+            _shootPos = gunTransform.position;
+            _shootShipSpeed = 0;
+
+            StartCoroutine(DestroyByDistance());
+        }
+
+        public override void OnHit()
+        {
+            PlayrParticlesAndDie();
+        }
+
+        private IEnumerator DestroyByDistance()
+        {
+            yield return new WaitUntil(() => SchouldNukeMySelf);
+
+            Destroy(gameObject);
+        }
+
+        private void PlayrParticlesAndDie()
+        {
+            _particleSystem.transform.SetParent(null);
+            _particleSystem.Play();
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Code/Gameplay/Enemies/Derived/TargetDummy.cs
+++ Assets/Code/Gameplay/Enemies/Derived/TargetDummy.cs
@@ -1,14 +1,28 @@
 using Game.Combat;
+using Game.Management;
+using Game.Player;
+using Game.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using static Zenject.CheatSheet;
 
 namespace Game.Room.Enemy
 {
     public class TargetDummy : EnemyBase
     {
+        [Inject] private PlayerManager _playerManager;
+
+        [SerializeField] private Transform _gunTransform;
+        [SerializeField] private EnemyBullet _enemyBullet;
+        [SerializeField] private float _cooldown = 2f;
         [SerializeField] private ParticleSystem _hitParticle;
         [SerializeField] private ParticleSystem _defeatParticle;
+
+        private float _lastShotTime = 0f;
+
+        private Vector2 PlayerPos => _playerManager.PlayerBody.position;
 
         protected override void Awake()
         {
@@ -19,6 +33,43 @@ namespace Game.Room.Enemy
             {
                 Debug.LogError($"_particle must be child of {transform.name}", gameObject);
             }
+        }
+
+        private void Update()
+        {
+            AimGun();
+
+            if (Vector2.Distance(transform.position, PlayerPos) <= _enemyBullet.MaxDistance)
+            {
+                TryShoot();
+            }
+        }
+
+        private void AimGun()
+        {
+
+            Vector2 gunPos = (Vector2)_gunTransform.position;
+            float angleDegrees = Utils.AngleDirected(gunPos, PlayerPos) - 90f;
+
+            Quaternion rotation = Quaternion.Euler(0, 0, angleDegrees);
+            _gunTransform.rotation = rotation;
+        }
+
+        private void TryShoot()
+        {
+            if (Time.time - _lastShotTime < _cooldown)
+            {
+                return;
+            }
+
+            Shoot();
+        }
+
+        private void Shoot()
+        {
+            _lastShotTime = Time.time;
+
+            _enemyBullet.CreateCopy().Shoot(null, _gunTransform);
         }
 
         public override void GetHit(Collision2D collsion, DamageData damage)
