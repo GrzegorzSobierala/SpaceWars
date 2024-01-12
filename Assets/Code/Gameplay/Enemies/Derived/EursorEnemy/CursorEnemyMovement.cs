c52ef4e031ee09572fe276c68644ca076e4e4189
Grzegorz Sobierała <gsobierala@gmail.com>
2024-01-12 21:31:06 +0100

    NavMesh2D test scene works

--- Assets/Code/Gameplay/Enemies/Derived/EursorEnemy/CursorEnemyMovement.cs
+++ Assets/Code/Gameplay/Enemies/Derived/EursorEnemy/CursorEnemyMovement.cs
@@ -7,5 +7,12 @@ namespace Game.Room.Enemy
     public class CursorEnemyMovement : EnemyMovementBase
     {
         public override bool UseFixedUpdate => true;
+
+        //[SerializeField] private ;
+
+        protected override void OnGoingTo(Transform fallowTarget)
+        {
+            base.OnGoingTo(fallowTarget);
+        }
     }
 }
