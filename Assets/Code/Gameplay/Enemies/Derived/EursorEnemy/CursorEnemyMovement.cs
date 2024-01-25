97d612b23c896587dff45f2a88fa2a087ea64fd0
Grzegorz Sobierała <gsobierala@gmail.com>
2024-01-25 22:37:44 +0100

    Room 1 test set up

--- Assets/Code/Gameplay/Enemies/Derived/EursorEnemy/CursorEnemyMovement.cs
+++ Assets/Code/Gameplay/Enemies/Derived/EursorEnemy/CursorEnemyMovement.cs
@@ -13,11 +13,73 @@ namespace Game.Room.Enemy
 
         [Inject] private NavMeshAgent _agent;
 
+        [SerializeField] private float _spotRange = 750;
+
+        private bool _isFallowing = false;
+
+        private float _nextStop = 0;
+        private float _stopTime = 0;
+        private float _currentStopTime = 0;
+
+        private void Start()
+        {
+            _agent.isStopped = true;
+        }
+
         protected override void OnGoingTo(Transform fallowTarget)
         {
             base.OnGoingTo(fallowTarget);
 
-            _agent.SetDestination(fallowTarget.transform.position);
+            if(!_isFallowing)
+            {
+                if (Vector2.Distance(transform.position, fallowTarget.position) < _spotRange)
+                {
+                    _agent.isStopped = false;
+                    _isFallowing = true;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if(_nextStop > Time.time)
+            {
+                _nextStop = Random.Range(10f, 30f) + Time.time;
+                _stopTime = Random.Range(1f, 9f);
+                _currentStopTime = Time.time;
+                _agent.isStopped = true;
+            }
+
+            if(_agent.isStopped && _currentStopTime + _stopTime > Time.time)
+            {
+                _agent.isStopped = false;
+            }
+
+            if(Time.frameCount % 25 == 0)
+            {
+                _agent.SetDestination(fallowTarget.transform.position);
+            }
+        }
+
+        private float CalculatePathLength(NavMeshPath path, Transform target)
+        {
+            Vector3[] pathCorners = new Vector3[path.corners.Length + 2];
+
+            // Get the corners of the path
+            pathCorners[0] = transform.position;
+            pathCorners[pathCorners.Length - 1] = target.position;
+            path.GetCornersNonAlloc(pathCorners);
+
+            float length = 0f;
+
+            // Sum the distances between consecutive corners
+            for (int i = 0; i < pathCorners.Length - 1; i++)
+            {
+                length += Vector3.Distance(pathCorners[i], pathCorners[i + 1]);
+            }
+
+            return length;
         }
     }
 }
