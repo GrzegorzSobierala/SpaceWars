2d67e11f917fb0b5a71eeec75e9d7ef36f616770
Grzegorz Sobierała <gsobierala@gmail.com>
2024-03-29 00:53:36 +0100

    Cursor running from while reloading

--- Assets/Code/Gameplay/Enemies/MainComponents/Derived/CursorEnemy/CursorEnemyCombatState.cs
+++ Assets/Code/Gameplay/Enemies/MainComponents/Derived/CursorEnemy/CursorEnemyCombatState.cs
@@ -1,6 +1,9 @@
 using Game.Management;
 using Zenject;
 using UnityEngine;
+using UnityEngine.AI;
+using Game.Utility;
+using System;
 
 namespace Game.Room.Enemy
 {
@@ -9,6 +12,12 @@ namespace Game.Room.Enemy
         [Inject] private CursorEnemyGun _gun;
         [Inject] private EnemyMovementBase _movement;
         [Inject] private PlayerManager _playerManager;
+        [Inject] private NavMeshAgent _agent;
+
+        [SerializeField] private float _minRunRange = 300;
+        [SerializeField] private float _maxRunRange = 1000;
+
+        [SerializeField] private float _runAngle = 45;
 
         protected override void OnEnterState()
         {
@@ -45,7 +54,41 @@ namespace Game.Room.Enemy
         private void RunFromPlayer()
         {
             _gun.StopAiming();
-            _movement.StartGoingTo(Vector2.zero);
+            StartMovingAwayFromPlayer();
+        }
+
+        private void StartMovingAwayFromPlayer()
+        {
+            Vector2 rayDir = transform.up * _maxRunRange;
+
+            float rotateMod = UnityEngine.Random.Range(0, 2) == 0 ? 1 : -1;
+            float rotateTargetPosAngle = _runAngle * rotateMod;
+
+            rayDir = Utils.RotateVector(rayDir, rotateTargetPosAngle);
+            Vector2 targetRayPos = (Vector2)transform.position + rayDir;
+
+            NavMeshHit hit;
+            _agent.Raycast(targetRayPos, out hit);
+            Debug.DrawLine(transform.position, hit.position, Color.green);
+
+
+            float agentSize = _agent.radius * 2;
+            if (agentSize > hit.distance)
+            {
+                _movement.StartGoingTo(transform.position);
+            }
+            else
+            {
+                Vector2 rayVector = (Vector2)hit.position - (Vector2)transform.position;
+                Vector2 targetPos = (Vector2)hit.position - (rayVector.normalized * agentSize);
+
+                Debug.DrawLine(transform.position, targetPos, Color.red);
+                _movement.StartGoingTo(targetPos);
+            }
+
+            _movement.SubscribeOnAchivedTarget(FallowPlayer);
+            Action unSubAction = () => _movement.UnsubscribeOnAchivedTarget(FallowPlayer);
+            _movement.SubscribeOnChangedTarget(unSubAction);
         }
     }
 }
