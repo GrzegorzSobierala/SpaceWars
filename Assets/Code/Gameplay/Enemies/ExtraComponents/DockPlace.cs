deb5016a1313d2d70046e7ad21b8040349986d54
Grzegorz Sobierała <gsobierala@gmail.com>
2024-04-18 01:49:29 +0200

    Same speed for diffrent rotation angles docking

--- Assets/Code/Gameplay/Enemies/ExtraComponents/DockPlace.cs
+++ Assets/Code/Gameplay/Enemies/ExtraComponents/DockPlace.cs
@@ -9,6 +9,7 @@ namespace Game.Room.Enemy
     {
         [SerializeField] private Transform _dockingPoint;
         [SerializeField] private float _dockingTime = 4;
+        [SerializeField] private float _rotSpeedMulti = 1.3f;
 
         private IDocking _occupand;
         private Coroutine _currentCoroutine;
@@ -76,13 +77,18 @@ namespace Game.Room.Enemy
         private IEnumerator MovingOperation(Vector2 startPos, float startRot,
             Vector2 endPos, float endRot, float endTime)
         {
+            float angleToRot = Mathf.DeltaAngle(startRot, endRot);
+            float rotMulti = Mathf.Abs(angleToRot) / 180;
+
             while (endTime > Time.time)
             {
                 float currentTime = Time.time - (endTime - _dockingTime);
                 float t = math.remap(0, _dockingTime, 0, 1, currentTime);
                 float posT = LerpX.GetSmooth(ref t, LerpX.SmoothType.Smootherstep);
-                float rotT = Mathf.Clamp(t * 2, 0, 1);
-                rotT = LerpX.GetSmooth(ref rotT, LerpX.SmoothType.Smoothstep);
+
+                float rotT = math.remap(0, _dockingTime * rotMulti, 0, 1, currentTime);
+                rotT = Mathf.Clamp(rotT * _rotSpeedMulti, 0, 1);
+                rotT = LerpX.GetSmooth(ref rotT, LerpX.SmoothType.Smootherstep);
 
                 Vector2 targetPos = Vector2.Lerp(startPos, endPos, posT);
                 float targetRot = Mathf.LerpAngle(startRot, endRot, rotT);
