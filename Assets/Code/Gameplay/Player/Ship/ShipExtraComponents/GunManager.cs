607f7720fa7feec69986ef7ab9d3046685b31616
Grzegorz Sobierała <gsobierala@gmail.com>
2024-06-05 01:14:48 +0200

    Class architecture for specialgun for player

---
+++ Assets/Code/Gameplay/Player/Ship/ShipExtraComponents/GunManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game.Player.Ship
+{
+    public class GunManager : MonoBehaviour
+    {
+        private IGun _currentGun;
+    }
+}
---
+++ Assets/Code/Gameplay/Player/Ship/ShipParts/SpecialGun/Module/Derived/FatSpecialGun.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Player.Ship
+{
+    public class FatSpecialGun : SpecialGunModuleBase
+    {
+        protected override void OnShoot()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
