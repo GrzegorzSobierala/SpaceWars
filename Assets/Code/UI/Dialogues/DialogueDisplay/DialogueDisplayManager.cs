231fdb2ba91aa8ba11f5ce12b84f2649d0063529
Masky <biloxwolf@gmail.com>
2024-10-20 13:53:41 +0200

    separated testing to different script

--- Assets/Code/UI/Dialogues/DialogueDisplay/DialogueDisplayManager.cs
+++ Assets/Code/UI/Dialogues/DialogueDisplay/DialogueDisplayManager.cs
@@ -1,9 +1,6 @@
-using Game.Input.System;
-using NaughtyAttributes;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Zenject;
 
 namespace Game.Dialogues
 {
@@ -36,49 +33,6 @@ namespace Game.Dialogues
             {
                 Debug.LogError("Trying to display dialogue while other is displayed.");
             }
-        }
-
-
-
-        //-----------------------------------------Testing
-        [HorizontalLine(color: EColor.Gray)]
-        [Header("Testing")]
-        [SerializeField] private DialogueSequence _hubSequence;
-        [SerializeField] private DialogueSequence _choiceSequence;
-        [SerializeField] private DialogueSequence _blockSequence;
-        [SerializeField] private DialogueSequence _backgroundSequence;
-        [Inject] private InputProvider _inputProvider;
-        private Action _onDialogueEnd;
-
-        [Button]
-        private void HubDialogueTest()
-        {
-            _onDialogueEnd = null;
-            _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Dialogues);
-            _onDialogueEnd += () => { _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Gameplay);  };
-            StartDialogue(_hubSequence, _onDialogueEnd);
-        }
-        [Button]
-        private void ChoiceDialogueTest()
-        {
-            _onDialogueEnd = null;
-            _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Dialogues);
-            _onDialogueEnd += () => { _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Gameplay); };
-            StartDialogue(_choiceSequence, _onDialogueEnd);
-        }
-        [Button]
-        private void BlockDialogueTest()
-        {
-            _onDialogueEnd = null;
-            _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Dialogues);
-            _onDialogueEnd += () => { _inputProvider.SwitchActionMap(_inputProvider.PlayerControls.Gameplay); };
-            StartDialogue(_blockSequence, _onDialogueEnd);
-        }
-        [Button]
-        private void BackgroundDialogueTest()
-        {
-            _onDialogueEnd = null;
-            StartDialogue(_backgroundSequence, _onDialogueEnd);
         }
     }
 }
