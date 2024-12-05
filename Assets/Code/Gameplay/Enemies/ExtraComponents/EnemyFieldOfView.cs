ad3fd45f7497bb419bd6a6bc2d8c88fbf8c502b8
Grzegorz Sobierała <gsobierala@gmail.com>
2024-12-05 15:52:26 +0100

    IgnoreColliders for FieldOfView

--- Assets/Code/Gameplay/Enemies/ExtraComponents/EnemyFieldOfView.cs
+++ Assets/Code/Gameplay/Enemies/ExtraComponents/EnemyFieldOfView.cs
@@ -1,3 +1,4 @@
+using AYellowpaper.SerializedCollections;
 using CodeMonkey.Utils;
 using Game.Management;
 using Game.Utility;
@@ -22,6 +23,7 @@ namespace Game.Room.Enemy
         [SerializeField] private float _fov = 90;
         [SerializeField] private int _rayCount = 2;
         [SerializeField] private float _viewDistance = 500f;
+        [SerializeField] private SerializedDictionary<Collider2D,OneEnum> _ignoreColliders;
 
         private float AngleIncrease => _fov / _rayCount;
         private const float PlayerCameraMaxViewDistance = 500f;
@@ -90,20 +92,37 @@ namespace Game.Room.Enemy
                 useTriggers = false,
                 layerMask = _allLayerMask,
                 useLayerMask = true,
+
             };
 
             for (int i = 0; i <= _rayCount; i++)
             {
-                Vector3 vertex;
                 Vector3 rayDirection = UtilsClass.GetVectorFromAngle(currentAngle + worldAngleAdd);
                 Vector3 origin = transform.position;
 
-                RaycastHit2D[] raycastHits = new RaycastHit2D[1];
+                RaycastHit2D[] raycastHits = new RaycastHit2D[_ignoreColliders.Count + 1];
 
-                int count = Physics2D.Raycast(origin, rayDirection, contactFilter,
-                    raycastHits, _viewDistance);
+                int hitsCount;
 
-                if (count == 0)
+
+                hitsCount = Physics2D.Raycast(origin, rayDirection, contactFilter,
+                raycastHits, _viewDistance);
+
+                RaycastHit2D? firstHit = null;
+
+                for (int j = 0; j < hitsCount; j++)
+                {
+                    if (_ignoreColliders.ContainsKey(raycastHits[j].collider))
+                    {
+                        continue;
+                    }
+
+                    firstHit = raycastHits[j];
+                    break;
+                }
+
+                Vector3 vertex;
+                if (firstHit == null)
                 {
                     Vector3 direction = UtilsClass.GetVectorFromAngle(currentAngle);
                     vertex = direction * _viewDistance;
@@ -117,12 +136,12 @@ namespace Game.Room.Enemy
                 }
                 else
                 {
-                    vertex = raycastHits[0].point - (Vector2)transform.position;
+                    vertex = firstHit.Value.point - (Vector2)transform.position;
                     vertex = Utils.RotateVector(vertex, -worldAngleAdd);
 
-                    if (IsTargetFound(raycastHits[0]))
+                    if (IsTargetFound(firstHit.Value))
                     {
-                        OnTargetFound?.Invoke(raycastHits[0].collider.gameObject);
+                        OnTargetFound?.Invoke(firstHit.Value.collider.gameObject);
                     }
                 }
 
--- Assets/Code/Gameplay/Enemies/MainComponents/Derived/StationEnemy/StationEnemyGuardState.cs
+++ Assets/Code/Gameplay/Enemies/MainComponents/Derived/StationEnemy/StationEnemyGuardState.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 namespace Game.Room.Enemy
 {
     public class StationEnemyGuardState : EnemyGuardStateBase
     {
+        [Inject] private EnemyBase enemyBase;
+
+        protected override void OnEnterState()
+        {
+            base.OnEnterState();
+
+            foreach (var gun in enemyBase.GetComponentsInChildren<BasicEnemyGun>())
+            {
+                gun.StartAimingAt(_playerManager.PlayerBody.transform);
+                gun.StartShooting();
+            }
+        }
+
 
 
 
