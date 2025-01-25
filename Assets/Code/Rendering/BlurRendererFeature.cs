f5b8d2dc6889115a8c971c032bf701522e2e5db9
Grzegorz Sobierała <gsobierala@gmail.com>
2025-01-25 03:58:20 +0100

    Blur render feature done

--- Assets/Code/Rendering/BlurRendererFeature.cs
+++ Assets/Code/Rendering/BlurRendererFeature.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.Profiling;
 using UnityEngine.Rendering;
@@ -11,204 +12,115 @@ namespace Game.Rendering
     {
         class BlurLayerRenderPass : ScriptableRenderPass
         {
-            private readonly ShaderTagId shaderTag = new ShaderTagId("UniversalForward");
-            private readonly int blurLayerMask;
-            private string profilerTag;
-            BlurRendererFeature feature;
-
-            int lastCheckFrame = 0;
-            int checkFrameCount = 0;
-
-            private int CheckFrameCount()
-            {
-                if (lastCheckFrame == Time.frameCount)
-                {
-                    checkFrameCount++;
-                }
-                else
-                {
-                    lastCheckFrame = Time.frameCount;
-                    checkFrameCount = 1;
-                }
-
-                return checkFrameCount;
-            }
-
+            private readonly ShaderTagId _shaderTag = new ShaderTagId("UniversalForward");
+            private readonly int _blurLayerMask;
+            private string _profilerTag;
+            private BlurRendererFeature _feature;
+            private RTHandle _rtHandle;
 
             public BlurLayerRenderPass(string profilerTag, int layerMask, BlurRendererFeature feature)
             {
-                this.profilerTag = profilerTag;
-                this.blurLayerMask = layerMask;
-                this.feature = feature;
-                //RTHandle colorRT = RTHandles.Alloc(feature.blurRenderTexture)
-            }
+                this._profilerTag = profilerTag;
+                this._blurLayerMask = layerMask;
+                this._feature = feature;
 
-            CameraType GetCameraType()
-            {
-                return Camera.current == null ? CameraType.Game : Camera.current.cameraType;
+                if (_feature._blurRenderTextureaaa)
+                {
+                    this._rtHandle = RTHandles.Alloc(feature._blurRenderTextureaaa);
+                }
+                else
+                {
+                    this._rtHandle = RTHandles.Alloc(Screen.width, Screen.height);
+                }
             }
 
             public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
             {
-                //GetRenderTextureContent(feature.blurRenderTexture);
-                //GetRenderTextureContent(feature.blurRenderTexture);
-                //GetRenderTextureContent(feature.debugRenderTexture);
-
-                //Debug.Log($"{GetCameraType()} {colorAttachment}");
-                Debug.Log($"1 {GetRenderTextureContent(feature.blurRenderTexture)}");
-                if (Camera.current && Camera.current.cameraType != CameraType.SceneView)
+                if (!ShouldCameraTypeDraw())
                 {
                     ResetTarget();
-                    Debug.Log($"2 {GetRenderTextureContent(feature.blurRenderTexture)}");
                     return;
                 }
-                Debug.Log($"2 {GetRenderTextureContent(feature.blurRenderTexture)}");
 
-                if (feature.blurRenderTexture == null)
+                if (!_rtHandle.rt || _rtHandle != _feature._blurRenderTextureaaa)
                 {
-                    CreateNewDebugRT(cameraTextureDescriptor);
-                }
-                else
-                {
-                    feature.blurRenderTexture.Release();
-                    feature.blurRenderTexture.descriptor = cameraTextureDescriptor;
-                    feature.blurRenderTexture.Create();
+                    if (_feature._blurRenderTextureaaa)
+                    {
+                        _rtHandle = RTHandles.Alloc(_feature._blurRenderTextureaaa);
+                    }
+                    else if (!_rtHandle.rt)
+                    {
+                        _rtHandle = RTHandles.Alloc(Screen.width, Screen.height);
+                        Debug.LogError("BlurRendererFeature is null, blur layer won't work", _feature);
+                    }
+                    else
+                    {
+                        Debug.LogError("BlurRendererFeature is null, blur layer won't work", _feature);
+                    }
                 }
 
-                
-                ConfigureTarget(feature.blurRenderTexture,feature.blurRenderTexture);
+                _rtHandle.rt.Release();
+                RenderTextureDescriptor descriptor = cameraTextureDescriptor;
+                descriptor.graphicsFormat = _feature._graphicFormat;
+                descriptor.depthStencilFormat = _feature._depthStencilFormat;
+                _rtHandle.rt.descriptor = descriptor;
+                _rtHandle.rt.Create();
+                //
+                ConfigureTarget(_rtHandle, _rtHandle);
                 ConfigureClear(ClearFlag.All, Color.clear);
             }
 
             public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
             {
+                CommandBuffer cmd = CommandBufferPool.Get(_profilerTag);
 
-                if (renderingData.cameraData.cameraType != CameraType.Game && renderingData.cameraData.cameraType != CameraType.SceneView)
+                if (!ShouldCameraTypeDraw())
                 {
+                    context.ExecuteCommandBuffer(cmd);
+                    CommandBufferPool.Release(cmd);
                     return;
                 }
 
-                CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
-
-                var drawingSettings = CreateDrawingSettings(shaderTag, ref renderingData, SortingCriteria.CommonOpaque);
-                var filteringSettings = new FilteringSettings(RenderQueueRange.opaque, blurLayerMask);
-
-                //cmd.SetGlobalTexture("_CameraDepthTexture", feature.blurRenderTexture);
+                var drawingSettings = CreateDrawingSettings(_shaderTag, ref renderingData, renderingData.cameraData.defaultOpaqueSortFlags);
+                var filteringSettings = new FilteringSettings(RenderQueueRange.opaque, _blurLayerMask);
 
                 context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings);
-
-                //cmd.Blit(feature.blurRenderTexture, feature.debugRenderTexture);
-
                 context.ExecuteCommandBuffer(cmd);
-
                 CommandBufferPool.Release(cmd);
-                //GetRenderTextureContent(feature.blurRenderTexture);
             }
 
-            public override void FrameCleanup(CommandBuffer cmd)
+            private CameraType GetCameraType()
             {
-                //GetRenderTextureContent(feature.blurRenderTexture);
-
-
-                if (Camera.current && Camera.current.cameraType != CameraType.SceneView)
-                    return;
-
-
-               // GetRenderTextureContent(feature.blurRenderTexture);
-               // GetRenderTextureContent(feature.debugRenderTexture);
-                //ResetTarget();
-                //Profiler.BeginSample($"{CheckFrameCount()} | Blur FrameCleanup {feature.blurRenderTexture.descriptor.width}");
-                //Profiler.EndSample();
+                return Camera.current == null ? CameraType.Game : Camera.current.cameraType;
             }
 
-            public override void OnFinishCameraStackRendering(CommandBuffer cmd)
+            private bool ShouldCameraTypeDraw()
             {
-                //GetRenderTextureContent(feature.blurRenderTexture);
-            }
-
-            public override void OnCameraCleanup(CommandBuffer cmd)
-            {
-                //GetRenderTextureContent(feature.blurRenderTexture);
-            }
-
-            public string GetRenderTextureContent(RenderTexture renderTexture)
-            {
-                // Ensure the RenderTexture is active
-                RenderTexture.active = renderTexture;
-
-                // Create a new Texture2D with the same dimensions as the RenderTexture
-                Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
-
-                // Read the pixels from the RenderTexture into the Texture2D
-                texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-                texture.Apply();
-
-                // Reset the active RenderTexture
-                RenderTexture.active = null;
-
-                //Debug.Log($"{GetCameraType()} {texture.imageContentsHash}");
-
-                return GetCameraType().ToString() + " | " + texture.imageContentsHash.ToString();
-            }
-
-            private void CreateNewDebugRT(RenderTextureDescriptor descriptor)
-            {
-                RenderTexture newDebugRT =
-                            new RenderTexture(descriptor);
-                newDebugRT.name = "BlurRenderTexture" + feature.name;
-                newDebugRT.Create();
-
-                string path = feature.DebugRenderTexturePath + "/" + newDebugRT.name + ".renderTexture";
-
-                // Save the RenderTexture as an asset in the project
-                UnityEditor.AssetDatabase.CreateAsset(newDebugRT, path);
-
-                feature.blurRenderTexture = newDebugRT;
-
-                Debug.Log($"RenderTexture created at: {path}");
-
-                UnityEditor.EditorUtility.SetDirty(feature.blurRenderTexture);
-                UnityEditor.AssetDatabase.SaveAssetIfDirty(feature.blurRenderTexture);
+                CameraType cameraType = GetCameraType();
+                return cameraType == CameraType.Game || cameraType == CameraType.SceneView;
             }
         }
 
-        [SerializeField] private LayerMask blurLayer;
-        [SerializeField] private RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
-        [Space]
-        public RenderTexture blurRenderTexture;
-        public RenderTexture debugRenderTexture;
+        [SerializeField] private LayerMask _blurLayer;
+        [SerializeField] private RenderPassEvent _renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;
+        [SerializeField] private RenderTexture _blurRenderTextureaaa;
+        [SerializeField] private GraphicsFormat _graphicFormat = GraphicsFormat.R8G8B8A8_UNorm;
+        [SerializeField] private GraphicsFormat _depthStencilFormat = GraphicsFormat.None;
 
-        public UnityEngine.Object debugRenderTexturePath;
-        public string DebugRenderTexturePath
-        {
-            get
-            {
-                string result = "Assets";
-
-#if UNITY_EDITOR
-                if (debugRenderTexturePath)
-                {
-                    result = UnityEditor.AssetDatabase.GetAssetPath(debugRenderTexturePath);
-                }
-#endif
-
-                return result;
-            }
-        }
-
-        private BlurLayerRenderPass blurLayerRenderPass;
+        private BlurLayerRenderPass _blurLayerRenderPass;
+        
 
         public override void Create()
         {
-            blurLayerRenderPass = new BlurLayerRenderPass("Render Blur Layer", blurLayer.value, this)
+            _blurLayerRenderPass = new BlurLayerRenderPass("Render Blur Layer", _blurLayer.value, this)
             {
-                renderPassEvent = this.renderPassEvent,
+                renderPassEvent = this._renderPassEvent,
             };
         }
 
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
-            renderer.EnqueuePass(blurLayerRenderPass);
+            renderer.EnqueuePass(_blurLayerRenderPass);
         }
     }
 }
