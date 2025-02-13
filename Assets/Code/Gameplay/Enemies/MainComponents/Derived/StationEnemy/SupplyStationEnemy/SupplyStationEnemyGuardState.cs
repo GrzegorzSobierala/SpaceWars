6ab274262390197a2cd324be67638f2046ae6474
Grzegorz Sobierała <gsobierala@gmail.com>
2025-02-13 11:18:46 +0100

    SupplyStation guard state

--- Assets/Code/Gameplay/Enemies/MainComponents/Derived/StationEnemy/SupplyStationEnemy/SupplyStationEnemyGuardState.cs
+++ Assets/Code/Gameplay/Enemies/MainComponents/Derived/StationEnemy/SupplyStationEnemy/SupplyStationEnemyGuardState.cs
@@ -1,9 +1,22 @@
-using UnityEngine;
+using System.Collections.Generic;
+using Zenject;
 
 namespace Game.Room.Enemy
 {
     public class SupplyStationEnemyGuardState : EnemyGuardStateBase
     {
+        [Inject] private List<EnemyFieldOfView> _views;
 
+        protected override void OnEnterState()
+        {
+            base.OnEnterState();
+            SubscribeToFOVs(_views);
+        }
+
+        protected override void OnExitState()
+        {
+            base.OnExitState();
+            UnubscribeToFOVs(_views);
+        }
     }
 }
