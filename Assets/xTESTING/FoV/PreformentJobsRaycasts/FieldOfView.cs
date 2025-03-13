d2e04364e6dc00fb5a4f6c6e3912bf3b3c84bda1
Grzegorz Sobierała <gsobierala@gmail.com>
2025-03-13 11:59:54 +0100

    RaycastsWithMesh

---
+++ Assets/xTESTING/FoV/PreformentJobsRaycasts/FieldOfView.cs
@@ -0,0 +1,365 @@
+using AYellowpaper.SerializedCollections;
+using CodeMonkey.Utils;
+using Game.Management;
+using Game.Utility;
+using Game.Utility.Globals;
+using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Profiling;
+using Unity.Jobs.LowLevel.Unsafe;
+using Zenject;
+
+namespace Game.Physics
+{
+    public class FieldOfView : MonoBehaviour
+    {
+        public Action<GameObject> OnTargetFound;
+
+        //[Inject] private PlayerManager _playerManager;
+        //[Inject] private Rigidbody2D _body;
+        //[Inject] private List<EnemyDamageHandler> _damageHandles;
+        //[Inject] private List<EnemyBase> _roomEnemies;
+
+        [SerializeField] private float _fov = 90;
+        [SerializeField] private int _rayCount = 2;
+        [SerializeField] private float _viewDistance = 500f;
+        [SerializeField] private bool _queriesStartInColliders = false;
+        [SerializeField] private SerializedDictionary<Collider2D, OneEnum> _ignoreColliders;
+        [SerializeField, Range(1,50)] private int _batchCount = 10;
+
+        private static HashSet<Collider2D> _DEBUG_wrongLayerColliders = new();
+
+        private float AngleIncrease => _fov / _rayCount;
+        private const float PlayerCameraMaxViewDistance = 500f;
+
+        private MeshFilter _meshFilter;
+        private Mesh _mesh;
+        private Collider2D _overlapCollider;
+
+        private LayerMask _allLayerMask;
+        private LayerMask _targetLayerMask;
+        private LayerMask _enemyLayerMask;
+        private ContactFilter2D _contactFilter;
+
+        private float _randomVertexZ;
+
+        private Vector2[] _pathPointsCompositeCache = new Vector2[10];
+        private List<Collider2D> _collidersCashe = new();
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void /*Fixed*/Update()
+        {
+            //if (!IsPlayerInRange() && !IsNonGuardEnemyInRange())
+            //    return;
+
+            UpdateView();
+        }
+
+        public void DrawViewGizmos()
+        {
+            //UpdateView(true);
+        }
+
+        private void Initialize()
+        {
+            _mesh = new Mesh();
+            _meshFilter = GetComponent<MeshFilter>();
+            _meshFilter.mesh = _mesh;
+            _targetLayerMask = LayerMask.GetMask(Layers.Player);
+            _allLayerMask = GetAllLayerMask();
+            _enemyLayerMask = LayerMask.GetMask(Layers.Enemy);
+            _randomVertexZ = UnityEngine.Random.Range(1.0f, 3.0f);
+            _overlapCollider = GetComponent<Collider2D>();
+
+            _contactFilter = new ContactFilter2D
+            {
+                useTriggers = false,
+                layerMask = _allLayerMask,
+                useLayerMask = true,
+            };
+
+            _mesh.uv = new Vector2[_rayCount + 1 + 1];
+        }
+
+        private void OnValidate()
+        {
+            if(!Application.isPlaying)
+                return;
+
+            _mesh.uv = new Vector2[_rayCount + 1 + 1];
+        }
+
+        private LayerMask GetAllLayerMask()
+        {
+            return LayerMask.GetMask(Layers.Player, Layers.Obstacle, Layers.Enemy);
+        }
+
+        private void UpdateView(/*bool debugMode = false*/)
+        {
+            Profiler.BeginSample("amigus1-1 over");
+            _overlapCollider.OverlapCollider(_contactFilter, _collidersCashe);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-2 list1");
+            NativeList<ColliderDataUnprepared> datasUnprep = new(_collidersCashe.Count, Allocator.TempJob);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-3 list2");
+            NativeList<Vector2> vertsUnprep = new(_collidersCashe.Count * 5, Allocator.TempJob);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-4 dataUnpare");
+
+            for (int i = 0; i < _collidersCashe.Count; i++)
+            {
+                Collider2D col = _collidersCashe[i];
+                Transform colTrans = col.transform;
+                switch (col)
+                {
+                    case BoxCollider2D box:
+                        Profiler.BeginSample("amigus1-4-1 dataUnpare box");
+
+                        ColliderDataUnprepared boxData = new()
+                        {
+                            typeEnum = ColliderType.Box,
+                            posWorld = colTrans.position,
+                            rotWorld = colTrans.eulerAngles.z,
+                            offsetLoc = box.offset,
+                            lossyScale = colTrans.lossyScale,
+                            sizeLoc = box.size
+                        };
+                        datasUnprep.Add(boxData);
+                        Profiler.EndSample();
+                        break;
+
+                    case CircleCollider2D circle:
+                        Profiler.BeginSample("amigus1-4-2 dataUnpare circle");
+
+                        ColliderDataUnprepared circleData = new()
+                        {
+                            typeEnum = ColliderType.Circle,
+                            posWorld = colTrans.position,
+                            rotWorld = colTrans.eulerAngles.z,
+                            offsetLoc = circle.offset,
+                            lossyScale = colTrans.localScale,
+                            radiusLoc = circle.radius
+                        };
+                        datasUnprep.Add(circleData);
+                        Profiler.EndSample();
+                        break;
+
+                    case CapsuleCollider2D capsule:
+                        Profiler.BeginSample("amigus1-4-3 dataUnpare capsule");
+                        ColliderDataUnprepared capsuleData = new()
+                        {
+                            typeEnum = ColliderType.Capsule,
+                            offsetLoc = capsule.offset,
+                            posWorld = colTrans.position,
+                            rotWorld = colTrans.eulerAngles.z,
+                            lossyScale = colTrans.lossyScale,
+                            sizeLoc = capsule.size,
+                            capsuleDirEnum = capsule.direction,
+                            capsuleTransUp = colTrans.up,
+                            capsuleTransRight = colTrans.right
+                        };
+                        datasUnprep.Add(capsuleData);
+                        Profiler.EndSample();
+                        break;
+
+                    case PolygonCollider2D poly:
+                        Profiler.BeginSample("amigus1-4-4 dataUnpare poly");
+                        Vector2[] points = poly.points;
+                        //Profiler.EndSample();
+
+                        //Profiler.BeginSample("amigus polygon 2");
+                        ColliderDataUnprepared polyData = new()
+                        {
+                            typeEnum = ColliderType.Polygon,
+                            vertexStartIndex = vertsUnprep.Length,
+                            posWorld = colTrans.position,
+                            rotWorld = colTrans.eulerAngles.z,
+                            lossyScale = colTrans.lossyScale,
+                            vertexCount = points.Length,
+                        };
+                        //Profiler.EndSample();
+
+                        //Profiler.BeginSample("amigus polygon 3");
+                        unsafe
+                        {
+                            fixed (Vector2* ptr = points)
+                            {
+                                vertsUnprep.AddRange(ptr, points.Length);
+                            }
+                        }
+
+                        //Profiler.EndSample();
+                        //Profiler.BeginSample("amigus polygon 4");
+                        datasUnprep.Add(polyData);
+                        Profiler.EndSample();
+                        break;
+
+                    case EdgeCollider2D edge:
+                        Profiler.BeginSample("amigus1-4-5 dataUnpare edge");
+                        Vector2[] edgePoints = edge.points;
+                        ColliderDataUnprepared edgeData = new()
+                        {
+                            typeEnum = ColliderType.Edge,
+                            vertexStartIndex = vertsUnprep.Length,
+                            posWorld = colTrans.position,
+                            rotWorld = colTrans.eulerAngles.z,
+                            lossyScale = colTrans.lossyScale,
+                            vertexCount = edgePoints.Length,
+                        };
+
+                        unsafe
+                        {
+                            fixed (Vector2* ptr = edgePoints)
+                            {
+                                vertsUnprep.AddRange(ptr, edgePoints.Length);
+                            }
+                        }
+
+                        datasUnprep.Add(edgeData);
+                        Profiler.EndSample();
+                        break;
+
+                    case CompositeCollider2D composite:
+                        Profiler.BeginSample("amigus1-4-6 dataUnpare composite");
+                        for (int p = 0; p < composite.pathCount; p++)
+                        {
+                            Profiler.BeginSample("amigus composite 1");
+                            int pointCount = composite.GetPathPointCount(p);
+                            Profiler.EndSample();
+
+                            Profiler.BeginSample("amigus composite 2");
+                            ColliderDataUnprepared compositeData = new()
+                            {
+                                typeEnum = ColliderType.Composite,
+                                vertexStartIndex = vertsUnprep.Length,
+                                posWorld = colTrans.position,
+                                rotWorld = colTrans.eulerAngles.z,
+                                vertexCount = pointCount,
+                            };
+
+                            Profiler.EndSample();
+
+                            Profiler.BeginSample("amigus composite 3");
+                            unsafe
+                            {
+                                if (_pathPointsCompositeCache.Length < pointCount)
+                                {
+                                    _pathPointsCompositeCache = new Vector2[pointCount];
+                                }
+                                composite.GetPath(p, _pathPointsCompositeCache);
+                                fixed (Vector2* pathPointsPtr = _pathPointsCompositeCache)
+                                {
+                                    vertsUnprep.AddRange(pathPointsPtr, pointCount);
+                                }
+                            }
+
+                            Profiler.EndSample();
+
+                            Profiler.BeginSample("amigus composite 4");
+                            datasUnprep.Add(compositeData);
+                            Profiler.EndSample();
+                        }
+                        Profiler.EndSample();
+                        break;
+
+                    default:
+                        ColliderDataUnprepared defaultData = new()
+                        {
+                            typeEnum = ColliderType.Unsuported,
+                            posWorld = col.bounds.center,
+                            sizeLoc = col.bounds.size,
+                        };
+                        datasUnprep.Add(defaultData);
+                        Debug.LogError("Unsuported collider type");
+                        break;
+
+                }
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-5 dataPrepare list1");
+            NativeArray<ColliderDataReady> datasRdy = new(datasUnprep.Length, Allocator.TempJob);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-6 dataPrepare list2");
+            NativeArray<float2> vertsRdy = new(vertsUnprep.Length, Allocator.TempJob);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-7-1 dataPrepare job");
+            PrepareColliderDatasJob prepareJob = new PrepareColliderDatasJob
+            {
+                datasUnprep = datasUnprep,
+                vertsUnprep = vertsUnprep,
+                datasRdy = datasRdy,
+                vertsRdy = vertsRdy,
+            };
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-7-2 dataPrepare job");
+            prepareJob.Run(datasRdy.Length);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-8 dataPrepare dispose");
+            datasUnprep.Dispose();
+            vertsUnprep.Dispose();
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus1-9 rayJob verticies array");
+            NativeArray<Vector3> verticies = new(_rayCount + 1 + 1, Allocator.TempJob);
+            verticies[0] = Vector3.zero;
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus2-2 rayJob triangles array");
+            NativeArray<int> triangles = new(_rayCount * 3, Allocator.TempJob);
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus2-3 rayJob create");
+            Raycast2DWithMeshJob raycastsJob = new Raycast2DWithMeshJob
+            {
+                rayOrigin = transform.position,
+                rayDistance = _viewDistance,
+                rayCount = _rayCount,
+                fovAnlge = _fov,
+                worldAngleAdd = transform.eulerAngles.z,
+                colliderDataArray = datasRdy,
+                vertexArray = vertsRdy,
+                verticies = verticies,
+                triangles = triangles,
+            };
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus2-4 rayJob run");
+            int batchCount = Mathf.Max(1, _rayCount / (JobsUtility.JobWorkerCount * 2));
+            JobHandle raycastsJobHandle = raycastsJob.Schedule(_rayCount, batchCount);
+            raycastsJobHandle.Complete();
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus2-5 mesh vertices");
+            _mesh.SetVertices(verticies);
+            Profiler.EndSample();
+            Profiler.BeginSample("amigus2-7 mesh triangles");
+            _mesh.triangles = triangles.ToArray();
+            //_mesh.SetTriangles()
+            Profiler.EndSample();
+
+            Profiler.BeginSample("amigus2-8 rayJob dispose");
+            datasRdy.Dispose();
+            vertsRdy.Dispose();
+            verticies.Dispose();
+            triangles.Dispose();
+            Profiler.EndSample();
+        }
+    }
+}
---
+++ Assets/xTESTING/FoV/PreformentJobsRaycasts/Raycast2DWithMeshJob.cs
@@ -0,0 +1,275 @@
+using CodeMonkey.Utils;
+using Game.Utility;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Game.Physics
+{
+    [BurstCompile(FloatMode = FloatMode.Fast, OptimizeFor = OptimizeFor.Performance, DisableDirectCall = true)]
+    public struct Raycast2DWithMeshJob : IJobParallelFor
+    {
+        public Vector2 rayOrigin;
+        public float rayDistance;
+        public int rayCount;
+        public float fovAnlge;
+        public float worldAngleAdd;
+
+        [ReadOnly] public NativeArray<ColliderDataReady> colliderDataArray;
+        // Contains vertices for all polygon/edge/composite colliders.
+        [ReadOnly] public NativeArray<float2> vertexArray;
+
+        [WriteOnly, NativeDisableParallelForRestriction] public NativeArray<Vector3> verticies;
+        [WriteOnly, NativeDisableParallelForRestriction] public NativeArray<int> triangles;
+
+        public void Execute(int index)
+        {
+            float startAngle = (fovAnlge / 2) + 90;
+            float currentAngle = startAngle - (((float)index / (float)rayCount) * fovAnlge);
+
+            Vector2 rayDirection = UtilsClass.GetVectorFromAngle(currentAngle + worldAngleAdd);
+
+            float minHitDistance = float.MaxValue;
+            Vector2 minHitPoint = Vector2.zero;
+
+            bool hitOnce = false;
+            for (int i = 0; i < colliderDataArray.Length; i++)
+            {
+                ColliderDataReady data = colliderDataArray[i];
+                float newHitDistance = float.MaxValue;
+                Vector2 newHitPoint = Vector2.zero;
+                bool hit = false;
+
+                switch (data.type)
+                {
+                    case (int)ColliderType.Box:
+                        hit = RayIntersectsBox(rayOrigin, rayDirection, rayDistance,
+                            data.center, data.rotationRad, data.size, out newHitDistance,
+                            out newHitPoint);
+                        break;
+
+                    case (int)ColliderType.Circle:
+                        hit = RayIntersectsCircle(rayOrigin, rayDirection, rayDistance, data.center,
+                            data.radius, out newHitDistance, out newHitPoint);
+                        break;
+
+                    case (int)ColliderType.Capsule:
+                        hit = RayIntersectsCapsule(rayOrigin, rayDirection, rayDistance, data.capsuleA,
+                            data.capsuleB, data.capsuleRadius, out newHitDistance, out newHitPoint);
+                        break;
+
+                    case (int)ColliderType.Polygon:
+                    case (int)ColliderType.Edge:
+                    case (int)ColliderType.Composite:
+                        hit = RayIntersectsPolygon(vertexArray, data.vertexStartIndex, data.vertexCount, data.isClosed,
+                            rayOrigin, rayDirection, rayDistance, out newHitDistance, out newHitPoint);
+                        break;
+                }
+
+                if (hit)
+                {
+                    hitOnce = true;
+                    if (newHitDistance < minHitDistance)
+                    {
+                        minHitDistance = newHitDistance;
+                        minHitPoint = newHitPoint;
+                    }
+                }
+            }
+
+            Vector3 vertex;
+            if (hitOnce)
+            {
+                vertex = minHitPoint - rayOrigin;
+                vertex = Utils.RotateVector(vertex, -worldAngleAdd);
+            }
+            else
+            {
+                Vector3 direction = UtilsClass.GetVectorFromAngle(currentAngle);
+                vertex = direction * rayDistance;
+            }
+
+            int vertexIndex = index + 1;
+            verticies[index + 1] = vertex;
+
+            if (index > 0)
+            {
+                int triangleIndex = index * 3;
+                triangles[triangleIndex] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                //triangleIndex += 3;
+            }
+        }
+
+        // --- Intersection routines ---
+
+        private bool RayIntersectsBox(Vector2 rayOrigin, Vector2 rayDir, float rayDist,
+                                        float2 boxCenter, float boxRotation, float2 boxSize,
+                                        out float hitDistance, out Vector2 hitPoint)
+        {
+            // Transform the ray into the box's local space.
+            float2 relativeOrigin = new float2(rayOrigin.x, rayOrigin.y) - boxCenter;
+            float cos = math.cos(-boxRotation);
+            float sin = math.sin(-boxRotation);
+            float2 localOrigin = new float2(relativeOrigin.x * cos - relativeOrigin.y * sin,
+                                           relativeOrigin.x * sin + relativeOrigin.y * cos);
+            float2 localDir = new float2(rayDir.x * cos - rayDir.y * sin,
+                                        rayDir.x * sin + rayDir.y * cos);
+
+            float2 extents = boxSize * 0.5f;
+            float2 invDir = new float2(1f / localDir.x, 1f / localDir.y);
+            float2 tMin = (-extents - localOrigin) * invDir;
+            float2 tMax = (extents - localOrigin) * invDir;
+            float2 t1 = math.min(tMin, tMax);
+            float2 t2 = math.max(tMin, tMax);
+            float tNear = math.max(t1.x, t1.y);
+            float tFar = math.min(t2.x, t2.y);
+
+            if (tNear > tFar || tFar < 0f || tNear > rayDist)
+            {
+                hitDistance = float.MaxValue;
+                hitPoint = float2.zero;
+                return false;
+            }
+
+            hitDistance = tNear;
+            float2 localHitPoint = localOrigin + localDir * tNear;
+            float cosR = math.cos(boxRotation);
+            float sinR = math.sin(boxRotation);
+            hitPoint = new float2(localHitPoint.x * cosR - localHitPoint.y * sinR,
+                                  localHitPoint.x * sinR + localHitPoint.y * cosR) + boxCenter;
+            return true;
+        }
+
+        private bool RayIntersectsCircle(Vector2 rayOrigin, Vector2 rayDir, float rayDist,
+                                           float2 circleCenter, float radius,
+                                           out float hitDistance, out Vector2 hitPoint)
+        {
+            float2 m = new float2(rayOrigin.x, rayOrigin.y) - circleCenter;
+            float b = math.dot(m, rayDir);
+            float c = math.dot(m, m) - radius * radius;
+
+            if (c > 0f && b > 0f)
+            {
+                hitDistance = float.MaxValue;
+                hitPoint = float2.zero;
+                return false;
+            }
+
+            float discr = b * b - c;
+            if (discr < 0f)
+            {
+                hitDistance = float.MaxValue;
+                hitPoint = float2.zero;
+                return false;
+            }
+
+            float t = -b - math.sqrt(discr);
+            if (t < 0f)
+                t = 0f;
+            if (t > rayDist)
+            {
+                hitDistance = float.MaxValue;
+                hitPoint = float2.zero;
+                return false;
+            }
+            hitDistance = t;
+            hitPoint = rayOrigin + rayDir * t;
+            return true;
+        }
+
+        private bool RayIntersectsCapsule(Vector2 rayOrigin, Vector2 rayDir, float rayDist,
+                                            float2 A, float2 B, float radius,
+                                            out float hitDistance, out Vector2 hitPoint)
+        {
+            bool hit = false;
+            hitDistance = float.MaxValue;
+            Vector2 tempHitPoint = Vector2.zero;
+
+            float tA, tB, tRect;
+            Vector2 ptA, ptB, ptRect;
+            bool hitA = RayIntersectsCircle(rayOrigin, rayDir, rayDist, A, radius, out tA, out ptA);
+            bool hitB = RayIntersectsCircle(rayOrigin, rayDir, rayDist, B, radius, out tB, out ptB);
+
+            // For the central part, define a box that represents the capsule's rectangle.
+            float2 rectCenter = (A + B) * 0.5f;
+            float rectRotation = math.atan2(B.y - A.y, B.x - A.x);
+            float2 rectSize = new float2(math.distance(A, B), 2f * radius);
+            bool hitRect = RayIntersectsBox(rayOrigin, rayDir, rayDist, rectCenter, rectRotation, rectSize, out tRect, out ptRect);
+
+            if (hitA && tA < hitDistance) { hitDistance = tA; tempHitPoint = ptA; hit = true; }
+            if (hitB && tB < hitDistance) { hitDistance = tB; tempHitPoint = ptB; hit = true; }
+            if (hitRect && tRect < hitDistance) { hitDistance = tRect; tempHitPoint = ptRect; hit = true; }
+
+            hitPoint = tempHitPoint;
+            return hit;
+        }
+
+        private bool RayIntersectsPolygon(NativeArray<float2> vertices, int startIndex, int count, int isClosed,
+                                          Vector2 rayOrigin, Vector2 rayDir, float rayDist,
+                                          out float hitDistance, out Vector2 hitPoint)
+        {
+            bool hitFound = false;
+            hitDistance = rayDist;
+            hitPoint = Vector2.zero;
+            for (int i = 0; i < count - 1; i++)
+            {
+                float t;
+                Vector2 pt;
+                if (RayIntersectsSegment(rayOrigin, rayDir, rayDist, vertices[startIndex + i], vertices[startIndex + i + 1], out t, out pt))
+                {
+                    if (t < hitDistance)
+                    {
+                        hitDistance = t;
+                        hitPoint = pt;
+                        hitFound = true;
+                    }
+                }
+            }
+            if (isClosed == 1 && count > 2)
+            {
+                float t;
+                Vector2 pt;
+                if (RayIntersectsSegment(rayOrigin, rayDir, rayDist, vertices[startIndex + count - 1], vertices[startIndex], out t, out pt))
+                {
+                    if (t < hitDistance)
+                    {
+                        hitDistance = t;
+                        hitPoint = pt;
+                        hitFound = true;
+                    }
+                }
+            }
+            return hitFound;
+        }
+
+        private bool RayIntersectsSegment(Vector2 rayOrigin, Vector2 rayDir, float rayDist,
+                                            float2 p0, float2 p1, out float t, out Vector2 pt)
+        {
+            float2 v = p1 - p0;
+            float d = rayDir.x * v.y - rayDir.y * v.x;
+            if (math.abs(d) < 1e-6f)
+            {
+                t = float.MaxValue;
+                pt = float2.zero;
+                return false;
+            }
+            t = ((p0.x - rayOrigin.x) * v.y - (p0.y - rayOrigin.y) * v.x) / d;
+            float u = ((p0.x - rayOrigin.x) * rayDir.y - (p0.y - rayOrigin.y) * rayDir.x) / d;
+
+            if (t >= 0f && t <= rayDist && u >= 0f && u <= 1f)
+            {
+                pt = rayOrigin + rayDir * t;
+                return true;
+            }
+            t = float.MaxValue;
+            pt = float2.zero;
+            return false;
+        }
+
+    }
+}
